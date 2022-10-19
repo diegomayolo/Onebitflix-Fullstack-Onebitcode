16907f619b3b0fd95fede19fc64ed7e3ab733dbd
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-10-19 20:17:09 -0300

    Criação do controller para categorias e inclusão da primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
 import express from 'express';
 import { adminJs, adminJsRouter } from './adminjs';
 import { sequelize } from './database';
+import { router } from './routes';
 
 const app = express();
 
 app.use( express.static( 'public' ) );
 app.use( adminJs.options.rootPath, adminJsRouter );
+app.use( router );
 
 const PORT = process.env.PORT || 3000;
 
 app.listen( PORT, () => 
 {
-   sequelize.authenticate().then( () =>{
-      console.log("Connection has been established successfully.");
-   });
+   sequelize.authenticate().then( () => {
+      console.log( "Connection has been established successfully." );
+   } );
    
    console.log( `Server running on port ${PORT}` );
 } );

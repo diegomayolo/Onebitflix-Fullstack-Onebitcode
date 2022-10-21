81a9ac9b16d2b38519f572fc54b5c8d99ac1f790
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-10-21 19:39:25 -0300

    Criação da rota e método para obter três cursos aleatórios em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,7 +2,25 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
-   // get a course by id
+   // GET /courses/featured -> get courses featured
+   featured: async ( req: Request, res: Response ) => {
+      try
+      {
+         const featuredCourses = await courseService.getRandomFeaturedCourses();
+
+         return res.json( featuredCourses );
+      } 
+      
+      catch ( error )
+      {
+         if ( error instanceof Error )
+         {
+            return res.status( 400 ).json( { message: error.message } );
+         }
+      }
+   },
+
+   // GET /courses/:id -> get a course by id with its episodes
    show: async ( req: Request, res: Response ) => {
       const { id } = req.params;
 
--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,10 @@ import { coursesController } from './controllers/coursesController';
 
 const router = express.Router();
 
-router.get('/categories', categoriesController.index );
+router.get('/categories',     categoriesController.index );
 router.get('/categories/:id', categoriesController.show );
 
-router.get('/courses/:id', coursesController.show );
+router.get('/courses/featured', coursesController.featured );
+router.get('/courses/:id',      coursesController.show );
 
 export { router };

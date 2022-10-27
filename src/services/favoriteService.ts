bafbb582edcda19a6964d6b43dd9367765a735dc
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-10-27 20:27:40 -0300

    Endpoint para excluir um curso da lista de favoritos de um determinado usuário

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -24,4 +24,10 @@ export const favoriteService = {
 
       return favorite;
    },
+
+   delete: async ( userId: number, courseId: number ) => {
+      await Favorite.destroy({
+         where: { userId, courseId }
+      });
+   }
 };

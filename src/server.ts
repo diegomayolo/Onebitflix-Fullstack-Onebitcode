9e6c1b75423a8c60cf4c85438d6473d5a6a7ab8f
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-11-08 20:22:38 -0300

    Adicionada a biblioteca cors para permitir requisições de origens diferentes

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,27 @@ import express from 'express';
 import { adminJs, adminJsRouter } from './adminjs';
 import { sequelize } from './database';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 
+/* Telling the server to use the public folder as the root folder. */
 app.use( express.static( 'public' ) );
+
+/* A middleware that allows cross-origin requests. */
+app.use( cors() );
 
 /* A middleware that parses the body of the request and makes it available in the request object. */
 app.use( express.json() );
 
+/* Telling the server to use the router. */
 app.use( router );
 
+/* Telling the server to use the adminJsRouter when the rootPath is used. */
 app.use( adminJs.options.rootPath, adminJsRouter );
 
+/* Setting the port to the environment variable PORT or 3000 if the environment variable PORT is not
+set. */
 const PORT = process.env.PORT || 3000;
 
 app.listen( PORT, () => 

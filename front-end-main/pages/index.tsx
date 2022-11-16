b9a432cc17d345c6077e86d9a9cad72592875071
Diego Luis Mayolo <diego.mayolo@outlook.com>
2022-11-16 19:37:22 -0300

    Configurando pastas, components e variáveis globais

--- front-end-main/pages/index.tsx
+++ front-end-main/pages/index.tsx
@@ -5,7 +5,10 @@ const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title"/>
+        <meta name='description' content='Tenha acesso aos melhores cursos de programação de uma forma simples e fácil!'/>
       </Head>
       <main></main>
     </>
